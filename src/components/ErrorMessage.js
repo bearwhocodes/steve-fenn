3512d09a623d198ec5f967c9c99469b7357a654e
Steve Fenn <steve@bearwho.codes>
2018-03-09 21:16:28 +0000

    Changing error message

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const ErrorMessage = ({ error }) => (
   <div>
     <h1>Oops something has gone wrong!</h1>
-    <p>{this.props.error}</p>
+    <p>{error}</p>
     <p>
       <Link to="/">Go Home</Link>
     </p>
